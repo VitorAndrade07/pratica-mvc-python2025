0ad5b8d5f255fe95a1325fe4bc02925be042e7d9
Vitor Costa de Andrade <159261494+VitorAndrade07@users.noreply.github.com>
2025-03-31 15:28:33 -0300

    Add files via upload

---
+++ pratica-mvc-python/model/tarefa.py
@@ -0,0 +1,40 @@
+from model.database import Database
+ 
+class Tarefa:
+    def __init__(self, titulo, id=None,  data_conclusao=None):
+        self.id = id
+        self.titulo = titulo
+        self.data_conclusao = data_conclusao
+ 
+    def salvarTarefa(self):
+        """Salva uma nova tarefa no banco de dados."""
+        db = Database()
+        db.conectar()
+ 
+        sql = 'INSERT INTO tarefa (titulo, data_conclusao) VALUES (%s, %s)'
+        params = (self.titulo, self.data_conclusao)
+        db.executar(sql, params)
+        db.desconectar()
+ 
+    @staticmethod # decorador
+    def listarTarefas():
+        """Retornar uma lista com todas as tarefas cadastradas."""
+        db = Database()
+        db.conectar()
+ 
+        sql = 'SELECT id, titulo, data_conclusao FROM tarefa'
+        tarefas = db.consultar(sql)
+        db.desconectar()
+        return tarefas if tarefas else []
+
+    @staticmethod
+    def apagarTarefa(idTarefa):
+        """Apaga uma tarefa cadastrada no banco de dados."""
+        db = Database()
+        db.conectar()
+ 
+        sql = 'DELETE FROM tarefa WHERE id = %s'
+        params = (idTarefa,) # Precisa passar como tupla? (a, b, c, ...) SIM!
+        db.executar(sql, params)
+        db.desconectar()
+ 
